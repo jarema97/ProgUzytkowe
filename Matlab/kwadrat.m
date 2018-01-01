30faaf7393e48bb7763c6f080bf7fb29dc7cb83e
jarema97 <32666935+jarema97@users.noreply.github.com>
2018-01-01 18:33:19 +0100

    Update kwadrat.m

--- Matlab/kwadrat.m
+++ Matlab/kwadrat.m
@@ -1,10 +1,25 @@
 function [ y ] = kwadrat( x, a, b, c )
+%%[y]=kwadrat(a,b,c)
+%%funkcja oblicza wartoÅ›Ä‡ funkcji kwadratowej ax^2+bx+c
 
-% funkcja oblicza wartoœæ funkcji kwadratowej 
-% ax^2 + bx + x
+if(nargin<2)
 
-y = a*x^2 + b * x + c 
-
+%nie podano a,b,c
+a=1;
+b=0;
+c=0;
+elseif(nargin<3)
+%nie podano b,c
+b=0;
+c=0;
+elseif(nargin<4)
+%nie podano c
+c=0;
 
 end
+
+y = a * x^2 + b * x + c;
+
+end
+
 
